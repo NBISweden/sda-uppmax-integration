766d1cd2ccb5f0cbade56191036882fe71c693ff
dbampalikis <dimitris.bampalikis@gmail.com>
2022-03-04 10:16:29 +0100

    Create basic structure for JSON response

--- token/token.go
+++ token/token.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"time"
 
 	"github.com/NBISweden/sda-uppmax-integration/helpers"
 )
@@ -13,6 +14,14 @@ import (
 type tokenRequest struct {
 	Swamid    string `json:"swamid"`
 	ProjectID string `json:"projectid"`
+}
+
+type tokenResponse struct {
+	Swamid      string    `json:"swamid"`
+	ProjectID   string    `json:"projectid"`
+	RequestTime time.Time `json:"request_time"`
+	Expiration  time.Time `json:"expiration"`
+	S3Config    string    `json:"s3config"`
 }
 
 func readRequestBody(body io.ReadCloser) (tokenRequest tokenRequest, err error) {
@@ -33,6 +42,24 @@ func readRequestBody(body io.ReadCloser) (tokenRequest tokenRequest, err error) 
 	}
 
 	return tokenRequest, nil
+}
+
+func createS3Config(s string) (s3config string, expiration time.Time, err error) {
+	panic("unimplemented")
+}
+
+func createResponse(tokenRequest tokenRequest) (tokenResponse tokenResponse, err error) {
+
+	tokenResponse.RequestTime = time.Now()
+	tokenResponse.Swamid = tokenRequest.Swamid
+	tokenResponse.ProjectID = tokenRequest.ProjectID
+
+	tokenResponse.S3Config, tokenResponse.Expiration, err = createS3Config(tokenRequest.Swamid)
+	if err != nil {
+		return tokenResponse, fmt.Errorf("Error creating S3 configuration")
+	}
+
+	return tokenResponse, err
 }
 
 func GetToken(w http.ResponseWriter, r *http.Request) {
